1cb32be3ead64efe31a617bddf3e01c8bcd335b4
AgentGecko <danica.pivalica@gmail.com>
2014-07-21 23:38:09 +0200

    Wanna be ViewModel

---
+++ GetSlides.WEB/Scripts/Source/TypeScript.ts
@@ -0,0 +1,39 @@
+﻿/// <reference path="knockout.d.ts" />
+module PresentationViewModel {
+
+    export interface IPresentation {
+        Id: number;
+    }
+
+    // Class to represent a presentation 
+    export class Presentation implements IPresentation {
+        Id: number;
+        UserId: number;
+        ImgUri: any;
+        Info: string;
+        DateUploaded: number;
+
+        getID() {
+            return this.Id;
+        }
+        setID(newID: number) {
+            this.Id = newID;
+        }
+        getUserID() {
+            return this.UserId;
+        }
+
+    }
+
+    // Overall ViewModel for one user's screen
+    export class PresetationViewModel {
+        self: any = this;
+        UserId: number;
+
+        UserPresentations: KnockoutObservableArray<Presentation>;
+
+        
+        
+
+    }
+}
---
+++ GetSlides.WEB/Scripts/app.js
@@ -0,0 +1,30 @@
+﻿/// <reference path="knockout.d.ts" />
+var PresentationViewModel;
+(function (PresentationViewModel) {
+    // Class to represent a presentation
+    var Presentation = (function () {
+        function Presentation() {
+        }
+        Presentation.prototype.getID = function () {
+            return this.Id;
+        };
+        Presentation.prototype.setID = function (newID) {
+            this.Id = newID;
+        };
+        Presentation.prototype.getUserID = function () {
+            return this.UserId;
+        };
+        return Presentation;
+    })();
+    PresentationViewModel.Presentation = Presentation;
+
+    // Overall ViewModel for one user's screen
+    var PresetationViewModel = (function () {
+        function PresetationViewModel() {
+            this.self = this;
+        }
+        return PresetationViewModel;
+    })();
+    PresentationViewModel.PresetationViewModel = PresetationViewModel;
+})(PresentationViewModel || (PresentationViewModel = {}));
+//# sourceMappingURL=app.js.map
