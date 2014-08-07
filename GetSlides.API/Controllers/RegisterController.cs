ad4e8fcbabcbc40c453f9ea49fe728fd14bf0c92
AgentGecko <danica.pivalica@gmail.com>
2014-08-07 11:07:23 +0200

    Login scripts

--- GetSlides.API/Controllers/RegisterController.cs
+++ GetSlides.API/Controllers/RegisterController.cs
@@ -11,18 +11,6 @@ namespace GetSlides.API.Controllers
     public class RegisterController : ApiController
     {
         [HttpPost]
-        public void Register(HttpRequestMessage request) 
-        {
-
-        }
-
-        [HttpGet]
-        public string Register(string poruka, bool auth)
-        {
-            if (auth) return poruka; else return null;
-        }
-
-        [HttpPost]
         public object Register(string username, string email, string password, string confirmPassword)
         {
             try
---
+++ GetSlides.WEB/Scripts/Source/Validation.ts
@@ -0,0 +1,46 @@
+﻿/// <reference path="../typings/jquery/jquery.d.ts" />
+/// <reference path="../typings/jquery.validation/jquery.validation.d.ts" />
+
+class Validate
+{
+    public static RegisterValidation()
+    {
+        $('.login-form').validate({
+            rules:
+            {
+                email: {
+                    required: true,
+                    email: true
+                },
+                username: {
+                    required: true
+                },
+                password1: {
+                    required: true,
+                    minlength: 6
+                },
+                password2: {
+                    required: true,
+                    equalTo: "input[name='password1']"
+                }
+            },
+            messages: {
+                email: {
+                    required: "Your email is required for registration.",
+                    email: "Your email address must be in the format of name@domain.com"
+                },
+                password1: {
+                    required: "You need to enter a password for Your GetSlides account.",
+                    minlength: "Your password must be at least 6 characters long."
+                },
+                password2: {
+                    required: "You need to confirm Your password.",
+                    equalTo: "Please enter the same password as above."
+                },
+                username: {
+                    required: "A username is required for registration."
+                }
+            }
+        });
+    }
+}
