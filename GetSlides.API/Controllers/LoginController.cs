c1bc045e116619ff51b25def11e84e3271087804
AgentGecko <danica.pivalica@gmail.com>
2014-09-30 01:01:48 +0200

    Login erased.

--- GetSlides.API/Controllers/LoginController.cs
+++ GetSlides.API/Controllers/LoginController.cs
@@ -14,21 +14,7 @@ namespace GetSlides.API.Controllers
         [HttpPost]
         public object Login(string email, string password) 
         {
-            UserRepository bllUserRepo = new UserRepository();
-            AuthenticationToken userAuthenticationToken = new AuthenticationToken(email);
-            AuthTokenRepository bllAuthTokenRepo = new AuthTokenRepository();
-            User currentUser = bllUserRepo.SelectByEmail(email);
-            if (Hash.ValidateContent(password, currentUser.PasswordHash))
-            {
-                bllAuthTokenRepo.Create(new AuthToken { 
-                                                        StartDateTime = DateTime.Now,
-                                                        Token = Hash.CreateHash(userAuthenticationToken.Token), 
-                                                        UserID = currentUser.ID,
-                                                        Timespan = 20
-                                                       });
-                return userAuthenticationToken.Token;
-            }
-            return false;
+            return null;
         }
      
     }
