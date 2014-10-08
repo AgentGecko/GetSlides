da0814ad2d3cf9bdd8df77000b681905d4138393
Marin Bozinovic <marin.bozinovic@fer.hr>
2014-10-08 12:26:03 +0200

    presentation uploader

--- GetSlides.API/Controllers/PresentationController.cs
+++ GetSlides.API/Controllers/PresentationController.cs
@@ -6,6 +6,9 @@ using System.Net.Http;
 using System.Web.Http;
 using GetSlides.API.Models;
 using Newtonsoft.Json;
+using System.Web;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace GetSlides.API.Controllers
 {
@@ -20,6 +23,31 @@ namespace GetSlides.API.Controllers
             lista.Add(new Presentation("Prezetacija4", "", "Info4", ""));
             lista.Add(new Presentation("Prezentacija5", "", "Info5", ""));
             return lista;
+        }
+        public async Task<HttpResponseMessage> UploadPresentation()
+        {
+            if(!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            string root = HttpContext.Current.Server.MapPath("~/Uploads");
+            var provider = new MultipartFileStreamProvider(root);
+            try
+            {
+                await Request.Content.ReadAsMultipartAsync(provider);
+
+                foreach (MultipartFileData pdfFile in provider.FileData)
+                {
+                    Trace.WriteLine(pdfFile.Headers.ContentDisposition.FileName);
+                    Trace.WriteLine("Server path: " + pdfFile.LocalFileName);
+                }
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception error)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, error);
+            }
         }
     }
 }
