6532202bafdfa09c1e0b6d2d7ee22de9c87d24b9
AgentGecko <danica.pivalica@gmail.com>
2015-01-04 20:36:35 +0100

    router.ts

---
+++ GetSlides.APP/Scripts/src/router.js
@@ -0,0 +1,78 @@
+﻿var GetSlides;
+(function (GetSlides) {
+    var Router = (function () {
+        function Router() {
+            this.baseAdress = "/api";
+        }
+        Router.prototype.getJsonAuth = function (adress, token, callback) {
+            $.ajax({
+                url: this.baseAdress + adress,
+                type: 'GET',
+                accepts: "application/json",
+                dataType: "application/json",
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("Authorization", token);
+                }
+            }).done(function (data) {
+                callback(null, data);
+            }).fail(function (jqXhr, textStatus, errorThrown) {
+                callback(errorThrown, null);
+            });
+        };
+
+        Router.prototype.postJsonAuth = function (adress, token, callback) {
+            $.ajax({
+                url: this.baseAdress + adress,
+                type: 'POST',
+                accepts: "application/json",
+                dataType: "application/json"
+            }).done(function (data) {
+                callback(null, data);
+            }).fail(function (jqXhr, textStatus, errorThrown) {
+                callback(errorThrown, null);
+            });
+        };
+
+        Router.prototype.getJson = function (adress, callback) {
+            $.ajax({
+                url: this.baseAdress + adress,
+                type: 'GET',
+                accepts: "application/json",
+                dataType: "application/json"
+            }).done(function (data) {
+                callback(null, data);
+            }).fail(function (jqXhr, textStatus, errorThrown) {
+                callback(errorThrown, null);
+            });
+        };
+
+        Router.prototype.postJson = function (adress, callback) {
+            $.ajax({
+                url: this.baseAdress + adress,
+                type: 'GET',
+                accepts: "application/json",
+                dataType: "application/json"
+            }).done(function (data) {
+                callback(null, data);
+            }).fail(function (jqXhr, textStatus, errorThrown) {
+                callback(errorThrown, null);
+            });
+        };
+
+        Router.prototype.getToken = function (username, password, callback) {
+            var data = { grant_type: "password", username: username, password: password };
+            $.ajax({
+                url: "/token",
+                type: 'POST',
+                accepts: "application/json",
+                dataType: "application/json",
+                data: data
+            }).always(function (response, error) {
+                callback(error, JSON.parse(response.responseText));
+            });
+        };
+        return Router;
+    })();
+    GetSlides.Router = Router;
+})(GetSlides || (GetSlides = {}));
+//# sourceMappingURL=router.js.map
---
+++ GetSlides.APP/Scripts/src/router.ts
@@ -0,0 +1,81 @@
+﻿module GetSlides {
+
+    export class Router {
+        
+        constructor(){}
+
+        public baseAdress: string = "/api";
+
+        public getJsonAuth(adress: string, token: string, callback: Function) {
+            $.ajax({
+                url: this.baseAdress + adress,
+                type: 'GET',
+                accepts: "application/json",
+                dataType: "application/json",
+                beforeSend(xhr) {
+                    xhr.setRequestHeader("Authorization", token);
+                }
+            }).done((data) => {
+                callback(null, data);
+            }).fail((jqXhr, textStatus, errorThrown) => {
+                callback(errorThrown, null);
+            });
+
+        }
+
+        public postJsonAuth(adress: string, token: string, callback: Function) {
+            $.ajax({
+                url: this.baseAdress + adress,
+                type: 'POST',
+                accepts: "application/json",
+                dataType: "application/json"
+            }).done((data) => {
+                    callback(null, data);
+                }).fail((jqXhr, textStatus, errorThrown) => {
+                    callback(errorThrown, null);
+                });
+
+        }
+
+        public getJson(adress: string, callback: Function) {
+            $.ajax({
+                url: this.baseAdress + adress,
+                type: 'GET',
+                accepts: "application/json",
+                dataType: "application/json"
+            }).done((data) => {
+                callback(null, data);
+            }).fail((jqXhr, textStatus, errorThrown) => {
+                callback(errorThrown, null);
+            });
+        }
+
+        public postJson(adress: string, callback: Function) {
+            $.ajax({
+                url: this.baseAdress + adress,
+                type: 'GET',
+                accepts: "application/json",
+                dataType: "application/json"
+            }).done((data) => {
+                callback(null, data);
+            }).fail((jqXhr, textStatus, errorThrown) => {
+                callback(errorThrown, null);
+            });
+        }
+
+        public getToken(username: string, password: string, callback: Function) {
+            var data = { grant_type: "password", username: username, password: password };
+            $.ajax({
+                url: "/token",
+                type: 'POST',
+                accepts: "application/json",
+                dataType: "application/json",
+                data: data
+            }).always((response, error) => {
+                callback(error, JSON.parse(response.responseText));
+            });
+        }
+
+    }
+
+}
